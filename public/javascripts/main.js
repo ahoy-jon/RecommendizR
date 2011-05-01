60bca5f2d694f15d8929256532582b3e353bb371
Jean-Baptiste Lemée <jblemee@yahoo.fr>
2011-05-01 21:43:13 +0200

    JS Refactor

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,5 @@
-require(["jquery"], function($) {
-
+require(["jquery", "functional"], function($) {
+   $(document).ready(function() {
+      Functional.install();
+   });
 });
--- public/javascripts/widgets/likedlist.js
+++ public/javascripts/widgets/likedlist.js
@@ -1,20 +1,22 @@
-require.def("widgets/likedlist", [], function() {
-
-   var onSuccess = new function(likedList){
-
+require.def("widgets/likedlist", ["jquery"], function($) {
+   var onSuccess = function(containerId, likedList){
+      if(likedList.length ==0){
+         $('#'+containerId).html('<p>No data.</p>');
+      }
    }
 
-   var onError = new function(){
-
+   var onError = function(containerId){
+      alert("error"+containerId);
+        $('#'+containerId).html('<p>error</p>');
    }
 
    return {
-      "Instance": function (containerId, resource, limit) {
+      "Instance": function (containerId, resource, data) {
          $.ajax({
             url: resource,
-            data: {'limit':limit},
-            success: onSuccess,
-            error: onError
+            data: data,
+            success: onSuccess.curry(containerId),
+            error: onError.curry(containerId)
          });
       }
    };
